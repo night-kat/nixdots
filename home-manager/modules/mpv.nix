2dc1c2a16161fca2103615b7cbeecb77a4586696
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-07 16:42:13 +0100

    Fix MPV forcing HDR when going into fullscreen
    Others had this issue too. A github issue was created. Hopefully it will
    be fixed. This fix is only temporary. See:
    https://github.com/hyprwm/Hyprland/discussions/12775

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -18,6 +18,12 @@
     );
 
     config = {
+      # temporary fix for mpv enabling hdr in fullscreen mode
+      # if HDR is on, it causes everything to look dull. 
+      target-colorspace-hint-mode= "source";
+      # perfect settings for framework laptop
+      contrast = "-32";
+      brightness = "8"; 
       profile = "high-quality";
       ytdl-format = "bestvideo+bestaudio";
       cache-default = 4000000;
