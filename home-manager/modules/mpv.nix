e9a2d1a2eb961c82a9240b7e2fb4b6d68c73ac64
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-07 17:44:45 +0100

    Fix MPV forcing HDR in fullscreen and change some settings

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -3,21 +3,18 @@
 {
   programs.mpv = {
     enable = true;
-
-    package = (
-      pkgs.mpv-unwrapped.wrapper {
-        scripts = with pkgs.mpvScripts; [
-          mpris
-          sponsorblock
-        ];
-
-        mpv = pkgs.mpv-unwrapped.override {
-          waylandSupport = true;
-        };
-      }
-    );
-
+    scripts = with pkgs.mpvScripts; [
+      mpris
+      sponsorblock
+    ];
+    
     config = {
+      # temporary fix for mpv enabling hdr in fullscreen mode
+      # if HDR is on, it causes everything to look dull. 
+      target-colorspace-hint-mode= "source";
+      # perfect settings for framework laptop
+      contrast = "-32";
+      brightness = "8"; 
       profile = "high-quality";
       ytdl-format = "bestvideo+bestaudio";
       cache-default = 4000000;
