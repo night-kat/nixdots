52865bf3c9941e6e52e3054b9e4b366788b02787
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-18 08:12:42 +0100

    Fix wallpaper
    New hyprland update changed wallpaper syntax. This is the fix

--- home-manager/modules/hyprland/hyprpaper.nix
+++ home-manager/modules/hyprland/hyprpaper.nix
@@ -1,14 +1,14 @@
 { config, lib, pkgs, ... }:
+
 {
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [
-        "/etc/nixos/home-manager/wallpapers/frieren-magical-5120x2880-15165.jpeg"
-      ];
-      wallpaper = [
-        "eDP-1, /etc/nixos/home-manager/wallpapers/frieren-magical-5120x2880-15165.jpeg"
-      ];
+      wallpaper = {
+        "monitor" = "eDP-1";
+        "path" = "${../../wallpapers/frieren-magical-5120x2880-15165.jpeg}";
+        "fit_mode" = "cover";
+      };
     };
   };
 }
