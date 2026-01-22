4d97450f160f4f90c2e91bfae9be72baebd5f4ef
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-22 13:18:33 +0100

    Remove duplicate user, reformat users.nix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -37,22 +37,6 @@
   # i18n.defaultLocale = "en_GB.UTF-8";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nightcat = {
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "networkmanager"
-      "video"
-      "input"
-    ]; # Enable ‘sudo’ for the user.
-    # openssh.authorizedKeys.keys = [
-    # TODO: Add SSH public keys here
-    # ];
-    shell = pkgs.fish;
-    packages = with pkgs; [
-      tree
-    ];
-  };
 
   nixpkgs.config.allowUnfreePredicate =
     pkg:
--- hosts/laptop/users.nix
+++ hosts/laptop/users.nix
@@ -4,9 +4,14 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nightcat = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "video" "input" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+      "input"
+    ]; # Enable ‘sudo’ for the user.
     # openssh.authorizedKeys.keys = [
-        # TODO: Add SSH public keys here
+    # TODO: Add SSH public keys here
     # ];
     shell = pkgs.fish;
     packages = with pkgs; [
