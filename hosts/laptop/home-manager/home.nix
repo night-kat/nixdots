8a8f00add38819a1439dcfdbc5eda993602b118c
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-22 13:15:34 +0100

    Remove unused lines

--- hosts/laptop/home-manager/home.nix
+++ hosts/laptop/home-manager/home.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-     ./modules/default.nix
-  ];
+  # imports = [
+  #   ./modules/default.nix
+  # ];
   home.username = "nightcat";
   home.homeDirectory = "/home/nightcat";
   home.stateVersion = "25.05"; # Don't change this
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
