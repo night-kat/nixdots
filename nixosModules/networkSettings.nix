ddcd22ab8d2e33b86738f08faa076a95be73ab30
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-23 15:32:42 +0100

    Add networkSettings module

--- nixosModules/networkSettings.nix
+++ nixosModules/networkSettings.nix
@@ -1,12 +1,16 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   option = {
-    networkSettings.enable = lib.mkEnableOption "Enable common network settings"
-  }
-  networking.hostName = "nightcat"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+    custom.networkSettings.enable = 
+      lib.mkEnableOption "Enable common network settings"
+  };
 
+  config = lib.mkIf config.custom.networkSettings.enable {
+    # TODO: make on per host basis
+    # networking.hostName = "nightcat"; # Define your hostname.
+    # Pick only one of the below networking options.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  };
 }
