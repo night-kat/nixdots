c08e3783ba3b87a7f87a02ccb7bc7ab824afb176
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-23 15:44:35 +0100

    Rename overlays.nix to lix.nix to better reflect it's purpose

--- nixosModules/lix.nix
+++ nixosModules/lix.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
+
 {
-  nixpkgs.overlays = [
+  options = {
+    custom.enablelix.enable = 
+      lib.mkEnableOption "Enable the lix language, a community driven implementation of nix"
+  };
+
+  config = lib.mkIf config.custom.lix.enable {
+      nixpkgs.overlays = [
     (final: prev: {
       inherit (prev.lixPackageSets.stable)
         nixpkgs-review
@@ -12,4 +19,5 @@
   ];
 
   nix.package = pkgs.lixPackageSets.stable.lix;
+  };
 }
