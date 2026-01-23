a0fa3a50423b9214ca31d3fcfb3c1507fb390102
night-kat <178902183+night-kat@users.noreply.github.com>
2026-01-23 15:46:00 +0100

    Make nix settings module
    This only enables flakes for now, but it may do more in the future

--- nixosModules/nixSettings.nix
+++ nixosModules/nixSettings.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  option = {
+    custom.nixSettings.enable =
+      lib.mkEnableOption "Enable flakes" # This file may do more in the future
+  };
+
+  config = lib.mkIf config.custom.nixSettings.enable {
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
