7514b320e3742f0ace0eb6bcccd254c3a80768d6
alfarel <alfarelcynthesis@proton.me>
2026-01-23 17:17:58 +0000

    feat: (mostly) unobtrusive suggestions
    This is just the raw option changes, I'll include my notes in another
    commit or in pr comments.
    
    Functionality changes:
    
    - corrected laptop host-specific import path
    - add specialArgs and extraSpecialArgs for convenience, you can remove these
    - merged and moved home-manager nixos module options into a shared module
    
    The rest of the changes should not change functionality at all, and
    should be fairly unobtrusive.
    I'll explain all of them separately.

--- flake.nix
+++ flake.nix
@@ -14,17 +14,16 @@
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            ./laptop/configuration.nix
+            ./hosts/laptop/configuration.nix
             home-manager.nixosModules.home-manager
             {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.nightcat = ./home-manager/home.nix;
+              home-manager.extraSpecialArgs = { inherit inputs; };
 
-              # Optionally, use home-manager.extraSpecialArgs to pass
-              # arguments to home.nix
             }
           ];
+          specialArgs = {
+            inherit inputs;
+          };
         };
       };
     };
--- hosts/laptop/home-manager/home.nix
+++ hosts/laptop/home-manager/home.nix
@@ -1,11 +1,7 @@
-{ config, pkgs, ... }:
-
 {
   # imports = [
   #   ./modules/default.nix
   # ];
-  home.username = "nightcat";
-  home.homeDirectory = "/home/nightcat";
   home.stateVersion = "25.05"; # Don't change this
   nix.settings.experimental-features = [
     "nix-command"
