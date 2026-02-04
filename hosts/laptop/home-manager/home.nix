da827203e434f6136ac79e2cd9eb3da87f76c433
night-kat <178902183+night-kat@users.noreply.github.com>
2026-02-04 21:45:04 +0100

    Change signing key

--- hosts/laptop/home-manager/home.nix
+++ hosts/laptop/home-manager/home.nix
@@ -15,7 +15,7 @@
     signing = {
       format = "ssh";
       signByDefault = true;
-      key = "256 SHA256:0q+vDpRlycQT9R5l0vRenFBJ2SDJnaXnLjq36vTfmqY 178902183+night-kat@users.noreply.github.com (ED25519-SK)";
+      key = "~/.ssh/id_ed25519_sk.pub";
     };
   };
   
