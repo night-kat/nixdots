09ec1798cfb5b54357be252a267f1595544fc563
night-kat <178902183+night-kat@users.noreply.github.com>
2026-02-06 09:18:03 +0100

    Minor restructuring

---
+++ homeManagerModules/programs/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  imports = [
+    ./bacon.nix
+    ./firefox.nix
+    ./fish.nix
+    ./git.nix
+    ./mpv.nix
+    ./programs.nix
+    ./ssh.nix
+    ./waybar.nix
+    ./yazi.nix
+  ];
+}
