3bb87dc870bd2e29049286011a83c2549704114f
night-kat <178902183+night-kat@users.noreply.github.com>
2026-02-10 17:05:55 +0100

    First test desktop configuration

---
+++ hosts/desktop/home-manager/scripts/users.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+{
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nightcat = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+      "input"
+    ]; # Enable ‘sudo’ for the user.
+    # openssh.authorizedKeys.keys = [
+    # TODO: Add SSH public keys here
+    # ];
+    shell = pkgs.fish;
+    packages = with pkgs; [
+      tree
+    ];
+  };
+}
