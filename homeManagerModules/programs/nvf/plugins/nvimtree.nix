55ceb814654b1c67205aa11c800576e2ae717c89
night-kat <178902183+night-kat@users.noreply.github.com>
2026-02-16 12:44:32 +0100

    Configure NvimTree functionality
    Nvimtree will now change its root when you change directory

--- homeManagerModules/programs/nvf/plugins/nvimtree.nix
+++ homeManagerModules/programs/nvf/plugins/nvimtree.nix
@@ -3,5 +3,16 @@
 {
   programs.nvf.settings.vim.filetree.nvimTree = {
     enable = true;
+    setupOpts = {
+      git.enable = true;
+      renderer.highlight_git = true;
+      update_focused_file = {
+        enable = true;
+        update_root = true;
+      };
+      # This will sync nvimtree root with current working directory
+      sync_root_with_cwd = true; 
+    };
   };
 }
+
