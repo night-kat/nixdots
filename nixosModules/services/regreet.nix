30305c9a5a661a19c368e35224885a72cdd2dd2d
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-12 19:45:36 +0100

    Make regreet better

--- nixosModules/services/regreet.nix
+++ nixosModules/services/regreet.nix
@@ -23,16 +23,46 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    # services.gnome = {
-    #   gcr-ssh-agent.enable = false;
-    # };
+    # required to have german layout in login screen
     environment.variables = {
       XKB_DEFAULT_LAYOUT = "de";
       XKB_DEFAULT_VARIANT = "nodeadkeys";
     };
+
     programs.regreet = {
       enable = true;
+      settings = {
+        theme = {
+          name = "catppuccin-gtk";
+          package = pkgs.catppuccin-gtk;
+        };
+        GTK = {
+          # Whether to use the dark theme
+          application_prefer_dark_theme = true;
+        };
+
+        appearance = {
+          # The message that initially displays on startup
+          greeting_msg = "Welcome back!";
+        };
+
+        widget.clock = {
+          # strftime format argument
+          # See https://docs.rs/jiff/0.1.14/jiff/fmt/strtime/index.html#conversion-specifications
+          format = "%a %H:%M";
+        };
+
+        # env = {
+        #   XKB_DEFAULT_LAYOUT = "de";
+        #   XKB_DEFAULT_VARIANT = "nodeadkeys";
+        # };
+
+        background = {
+          path = "${./../../wallpapers/frieren_1.jpeg}";
+        };
+      };
     };
+
     services = {
       greetd = {
         enable = true;
@@ -45,7 +75,7 @@ in {
           # Available values: "Fill", "Contain", "Cover", "ScaleDown" Refer to:
           # https://docs.gtk.org/gtk4/enum.ContentFit.html NOTE: This is ignored if ReGreet
           # isn't compiled with GTK v4.8 support.
-          fit = "Contain";
+          fit = "Cover";
 
           default_session =
             lib.mkIf cfg.hyprland.enable {
