ffa4d8adb15da65f044728965361f8390cbf2300
night-kat <178902183+night-kat@users.noreply.github.com>
2026-03-16 16:06:24 +0100

    Add sddm theme

--- nixosModules/services/sddm.nix
+++ nixosModules/services/sddm.nix
@@ -1,25 +1,32 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }: let
   cfg = config.custom.sddm;
+  # sddm-astronaut-custom = pkgs.sddm-astronaut.override {
+  #   embeddedTheme = "black_hole";
+  # };
 in {
   options.custom.sddm = {
-    enable = lib.mkEnableOption "Enable sddm display manager with gnome-keyring";
+    enable = lib.mkEnableOption "Enable sddm display manager";
   };
 
   config = lib.mkIf cfg.enable {
-    # services.gnome = {
-    #   gnome-keyring.enable = true;
-    #   gcr-ssh-agent.enable = false;
-    # };
-    # security.pam.services.login.enableGnomeKeyring = true;
-    services = {
-      displayManager.sddm = {
-        wayland.enable = true;
-        enable = true;
-      };
+    environment.systemPackages = with pkgs; [
+      sddm-sugar-dark
+      sddm-astronaut
+    ];
+
+    services.displayManager.sddm = {
+      enable = true;
+      theme = "sddm-astronaut-theme";
+      wayland.enable = true;
+      extraPackages = [
+        pkgs.sddm-sugar-dark
+        pkgs.sddm-astronaut
+      ];
     };
   };
 }
