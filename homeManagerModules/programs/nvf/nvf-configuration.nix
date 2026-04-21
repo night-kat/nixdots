6fb65ba6bf490b2367ad2777185df387a51b9af8
night-kat <178902183+night-kat@users.noreply.github.com>
2026-04-21 19:59:33 +0200

    fix(nvf): Updated syntax
    TThe option `vim.lsp.harper-ls.enable' defined in
    `/nix/store/52npcqkp5s9a4vvr59mngvizqdn08f7c-source/homeManagerModules/programs/nvf/nvf-configuration.nix'
    has been renamed to `vim.lsp.presets.harper.enable'.

--- homeManagerModules/programs/nvf/nvf-configuration.nix
+++ homeManagerModules/programs/nvf/nvf-configuration.nix
@@ -44,7 +44,7 @@ in {
             trouble.enable = true;
             otter-nvim.enable = true;
             nvim-docs-view.enable = true;
-            harper-ls.enable = true;
+            presets.harper.enable = true;
           };
 
           statusline.lualine = {
