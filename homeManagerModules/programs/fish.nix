b76b11b877c1904e0a50146c4a695b4e1554adb4
night-kat <178902183+night-kat@users.noreply.github.com>
2026-05-05 17:14:54 +0200

    Change fish option name, remove sytemLevelFish option, add macchina fetch script

--- homeManagerModules/programs/fish.nix
+++ homeManagerModules/programs/fish.nix
@@ -3,16 +3,16 @@
   lib,
   pkgs,
   ...
-}:
-
-{
+}: {
   options = {
-    custom.fishWithAliases.enable = lib.mkEnableOption "Enable fish shell with some aliases";
+    custom.fish.enable = lib.mkEnableOption "Enable fish shell with some aliases and macchina";
   };
 
-  config = lib.mkIf config.custom.fishWithAliases.enable {
+  config = lib.mkIf config.custom.fish.enable {
+    programs.macchina.enable = true;
     programs.fish = {
       enable = true;
+      shellInit = "macchina";
       shellAliases = {
         btw = "echo i use nixos btw";
         grep = "rg";
